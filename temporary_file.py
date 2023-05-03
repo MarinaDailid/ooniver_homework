8b4b90a9c9be00f51b0e624a9a6fd54b01089268
Marina <dailidmarina@mail.ru>
2023-05-03 17:43:58 +0300

    task2

--- temporary_file.py
+++ temporary_file.py
@@ -1,8 +1,25 @@
-name = input("Введите свое имя:")
-lastname = input("Введите свою фамилию:")
-work = input("Введите название места работы:")
-age = input("Введите свой возраст:")
-print(work)
+user = {
+    'name': '',
+    'lastname': '',
+    'work':'',
+    'age':'',
+}
+
+print("Данные пользователя 1")
+user['name'] = input("Введите свое имя:")
+user['lastname'] = input("Введите свою фамилию:")
+user['work'] = input("Введите название места работы:")
+user['age'] = input("Введите свой возраст:")
+print(user['work'])
 print('----------')
-print(lastname, name, ',', age)
-input()
+print(user['lastname'], user['name'], ',', user['age'])
+
+user2 = {}
+print("Данные пользователя 2")
+user2['name'] = input("Введите свое имя:")
+user2['lastname'] = input("Введите свою фамилию:")
+user2['work'] = input("Введите название места работы:")
+user2['age'] = input("Введите свой возраст:")
+
+avg = (int(user['age']) + int(user2['age'])) // 2
+print(avg)
