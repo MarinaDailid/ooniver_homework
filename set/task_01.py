81debff85cfcc38ad8effd9513a612a912f6ff1f
Marina <dailidmarina@mail.ru>
2023-05-24 17:40:31 +0300

    Homework

---
+++ set/task_01.py
@@ -0,0 +1,9 @@
+#Сгенирируйте список из ста случайных элементов элементов (от 10 до 99). Выведите только уникальные из них.
+from random import randint
+list = [0]*100
+for a in range(0,100):
+    b = randint(10, 99)
+    list[a] = b
+print(list)
+list_01 = set(list)
+print(list_01)
