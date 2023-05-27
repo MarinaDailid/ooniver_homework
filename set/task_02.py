3916b29a6aea76d4ddb0130eeddf76ff355969d8
Marina <dailidmarina@mail.ru>
2023-05-27 22:05:06 +0300

    homework

--- set/task_02.py
+++ set/task_02.py
@@ -3,12 +3,21 @@
 # только общие элементы и только различные.
 from random import randint
 list_01 = [0]*10
-for a in range(0,10):
+for a in range(0, 10):
     b = randint(0, 9)
     list_01[a] = b
 print(list_01)
 list_02 = [0]*10
-for a in range(0,10):
+for a in range(0, 10):
     b = randint(0, 9)
     list_02[a] = b
 print(list_02)
+intersection_list = set(list_01).intersection(list_02) # одновременно в 2 множествах
+print(intersection_list)
+difference_list = set(list_01).difference(list_02) # есть в 1 множестве, но нет во 2
+print(difference_list)
+intersection_list_2 = set(list_02).intersection(list_01) # одновременно в 2 множествах
+print(intersection_list_2)
+intersection_list_3 = set(list_01).symmetric_difference(list_02) # только разные значения
+print(intersection_list_3)
+
